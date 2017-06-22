a3a8c2505486904f7367110a00f11033482a33de
桥付 <jiangzimao@126.com>
2017-06-22 21:06:19 +0800

    代码调整优化

--- test/DcsRadioGroup.js
+++ test/DcsRadioGroup.js
@@ -7,31 +7,11 @@ class DcsRadioGroup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      checked: props.checked,
       value: props.value,
-      key: props.shortcutkey,
       id: props.id,
     };
     this.onChange = this.onChange.bind(this);
     this.onClick = this.onClick.bind(this);
-  }
-
-  componentWillMount() {
-    // console.log(`add checkbox ${this.state.id}`);
-    if (!context.shortcutKeyMap.has('_form_key')) {
-      context.shortcutKeyMap.set('_form_key', new Map());
-    }
-    const formKeyMap = context.shortcutKeyMap.get('_form_key');
-    formKeyMap.set(this.state.id, { key: this.state.key, fun: this.onChange });
-  }
-
-  componentWillUnmount() {
-    // console.log(`remove checkbox ${this.state.id}`);
-    if (!context.shortcutKeyMap.has('_form_key')) {
-      context.shortcutKeyMap.set('_form_key', new Map());
-    }
-    const formKeyMap = context.shortcutKeyMap.get('_form_key');
-    formKeyMap.delete(this.state.id);
   }
 
   onChange() {
